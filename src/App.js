7bde0133845a184066ac044c7d64b34cd257da4b
RedTricity <lara.mcintyre@outlook.com>
2025-02-27 17:26:34 +0000

    app

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ function App() {
         <Routes>
           <Route
             path="/"
-            element={entered ? <IntroPage /> : <AboutMe onEnter={() => setEntered(true)} />}
+            element={entered ? <AboutMe /> : <IntroPage onEnter={() => setEntered(true)} />}
           />
           <Route path="/about" element={<AboutMe />} />
           <Route path="/projects" element={<Projects />} />
